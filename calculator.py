333f345082f827ad47b31aa035021883de02a8cf
Amal-nellanhi <amalnellanhi@gmail.com>
2025-11-03 12:02:56 +0530

    Add initial calculator program

---
+++ calculator.py
@@ -0,0 +1,63 @@
+"""
+Simple Menu-Driven Calculator
+Part of Git & GitHub Hands-on Workshop
+Organized by Algon DC GCEK
+Date: November 8, 2025
+
+"""
+
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+# TODO: Implement this function
+def divide(a, b):
+    pass
+
+# TODO: Implement this function
+def power(a, b):
+    pass
+
+while True:
+    print("\n---- CALCULATOR MENU ----")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Power")
+    print("6. Exit")
+
+    choice = input("Enter your choice (1-6): ")
+
+    if choice == '6':
+        print("Exiting... Byeee !!")
+        break
+
+    if choice not in ['1', '2', '3', '4', '5']:
+        print("Invalid choice! Please try again.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input! Please enter numeric values.")
+        continue
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+
+    elif choice == '4':
+        print("Result:", divide(num1, num2))  
+    elif choice == '5':
+        print("Result:", power(num1, num2))   
